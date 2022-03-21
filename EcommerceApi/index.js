ceb355b0cd78d3d9fe6bcce0f0cc6ea3e2078c74
abeni16 <abeni3ase7@gmail.com>
2022-03-21 09:58:40 +0300

    final design and api

--- EcommerceApi/index.js
+++ EcommerceApi/index.js
@@ -8,18 +8,22 @@ const productRoute = require("./routes/productRoute");
 const orderRoute = require("./routes/orderRoute");
 const cartRoute = require("./routes/cartRoute");
 const authRoute = require("./routes/auth");
+const stripeRoute = require("./routes/stripe")
 
+const cors = require('cors')
 dotenv.config();
 app.use(express.json());
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("db connection succesfull"))
   .catch((err) => console.log(err));
+app.use(cors())
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/carts", cartRoute);
+app.use("/api/checkout",stripeRoute)
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("server is running");
